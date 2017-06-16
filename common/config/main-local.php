b06b6b34c5ae6daabfbd72bc98a0c67f85d678a3
Galifax <galifax94@gmail.com>
2017-06-16 18:32:09 +0300

    bd

--- common/config/main-local.php
+++ common/config/main-local.php
@@ -5,7 +5,7 @@ return [
             'class' => 'yii\db\Connection',
             'dsn' => 'mysql:host=localhost;dbname=remont',
             'username' => 'root',
-            'password' => 'root',
+            'password' => '',
             'charset' => 'utf8',
         ],
         'mailer' => [
